f5b078d47c5ae8b1ba19fce3c4fc10693939338a
tpywao <tpywao@gmail.com>
2016-06-15 04:30:05 +0900

    typoとurl.pyの古い記法の除去

--- lpau/urls.py
+++ lpau/urls.py
@@ -13,14 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
+from django.contrib.auth.views import login, logout
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^account/login/$', 'django.contrib.auth.views.login',
+    url(r'^account/login/$', login,
         {'template_name': 'accounts/login.html'}
         ),
-    url(r'^account/logout/$', 'django.contrib.auth.views.logout',
+    url(r'^account/logout/$', logout,
         {'template_name': 'accounts/logged_out.html'}, name='logout'),
 ]
