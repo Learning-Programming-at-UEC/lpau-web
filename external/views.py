1c7872d0b766f2254950fb6439793becbeab07a6
tpywao <tpywao@gmail.com>
2016-06-25 22:35:35 +0900

    Top画面(外部向け)とりあえずメニュー無効化

--- external/views.py
+++ external/views.py
@@ -1,5 +1,15 @@
-from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import ListView
 
-class IndexView(TemplateView):
+class IndexView(ListView):
     template_name = 'external/index.html'
+    context_object_name = 'menu_list'
+    menu_list = [
+      {'name': 'プログラミング教室憲章', 'link': '#', 'disable': True},
+      {'name': '書類ダウンロード', 'link': '#', 'disable': True},
+      {'name': '年間スケジュール', 'link': '#', 'disable': True},
+      {'name': '運営組織', 'link': '#', 'disable': True},
+      {'name': '教室情報', 'link': '#', 'disable': True},
+    ]
+
+    def get_queryset(self):
+        return self.menu_list
