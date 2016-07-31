1e3f48c7e529949876a946a9668970a59a501db6
tpywao <tpywao@gmail.com>
2016-07-31 13:28:22 +0900

    生徒の作品をアップロードできるようにした

--- lpau/urls.py
+++ lpau/urls.py
@@ -13,7 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.conf.urls import url, include
+from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
@@ -31,3 +33,5 @@ urlpatterns = [
     url(r'^membership/', include('membership.urls')),
 ] + staticfiles_urlpatterns()
 
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- membership/admin.py
+++ membership/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import Thread, Comment
+from .models import Thread, Comment, StudentWork
 
 
 admin.site.register(Thread)
 admin.site.register(Comment)
+admin.site.register(StudentWork)
